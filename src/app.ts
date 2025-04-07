02c3608d8af3ad7ace006801dabc360d5939a178
1Murphy1 <115133958+1Murphy1@users.noreply.github.com>
2025-04-07 16:29:48 +0700

    add const api from app.ts(16-18 line)

--- src/app.ts
+++ src/app.ts
@@ -8,14 +8,16 @@ import { userRoutes } from "./routes/userRoutes";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
+const api = "/api";
+
 connectDB();
 
 const app = express();
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("/api", pingRoutes);
-app.use("/api/user", userRoutes);
+app.use(`${api}/auth`, authRoutes);
+app.use(api, pingRoutes);
+app.use(` ${api}/user`, userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
